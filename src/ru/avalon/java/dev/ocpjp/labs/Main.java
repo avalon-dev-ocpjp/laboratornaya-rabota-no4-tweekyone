e3ece16bddf687227ef0ffa973c4497e07f7f493
Eugene Poniukhov <tweeky92@gmail.com>
2019-10-12 13:00:35 +0300

    v 0.2

--- src/ru/avalon/java/dev/ocpjp/labs/Main.java
+++ src/ru/avalon/java/dev/ocpjp/labs/Main.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.OptionalDouble;
+import java.util.OptionalInt;
 
 public class Main {
 
@@ -26,8 +27,8 @@ public class Main {
         
         //Самый дорогой товар
         OptionalDouble maxPrice = commodities.stream()
-                                            .mapToDouble(Commodity::getPrice)
-                                            .max();
+                                             .mapToDouble(Commodity::getPrice)
+                                             .max();
         System.out.println("Most expensive product:");
         commodities.stream()
                    .filter(x -> x.getPrice() == maxPrice.getAsDouble())
@@ -37,14 +38,58 @@ public class Main {
         System.out.println();
         
         //Товары с минимальным остатком
-         Optional<Integer> minResidue = commodities.stream()
-                                                    .map(Commodity::getResidue)
-                                                    .sorted()
-                                                    .findFirst();
-         System.out.println("minimum balance:");
-         commodities.stream()
-                    .filter(x -> x.getResidue() == (int)minResidue.get())
+        Optional<Integer> minResidue = commodities.stream()
+                                                  .map(Commodity::getResidue)
+                                                  .sorted()
+                                                  .findFirst();
+        System.out.println("Minimum balance:");
+        commodities.stream()
+                   .filter(x -> x.getResidue() == (int)minResidue.get())
+                   .map(Commodity::getName)
+                   .forEach(System.out::println);
+        
+        System.out.println();
+        
+        //Товары с остатком меньше 5
+        System.out.println("Goods with a balance of less than 5");
+        commodities.stream()
+                    .filter(x -> x.getResidue() < 5)
                     .map(Commodity::getName)
                     .forEach(System.out::println);
+        
+        System.out.println();
+        
+        //Подсчитать общую стоимость товаров с учетом их остатка
+        double totalPrice = commodities.stream()
+                                       .mapToDouble(x -> x.getPrice() * x.getResidue())
+                                       .sum();
+        System.out.println("Total price = " + (float) totalPrice);
+        
+        System.out.println();
+        
+        //Товар, с самым длинным названием
+        OptionalInt maxLength = commodities.stream()
+                                           .map(Commodity::getName)
+                                           .mapToInt(x -> x.length())
+                                           .max();
+        System.out.println("Longest name is:");
+        commodities.stream()
+                   .filter(x -> x.getName().length() == maxLength.getAsInt())
+                   .map(Commodity::getName)
+                   .forEach(System.out::println);
+        
+        System.out.println();
+        
+        //Сортировка коллекции commodities
+        commodities.stream().sorted();
+        
+        //Средняя стоимость товаров
+        OptionalDouble average = commodities.stream()
+                                            .mapToDouble(x -> x.getPrice())
+                                            .average();
+        
+        System.out.println("Average price of goods is " + (float) average.getAsDouble());
+        
+        System.out.println();
     }
 }
